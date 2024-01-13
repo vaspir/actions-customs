502dc4ada5e786571ab7fce1a3d634a14bd704ca
v.spirydzionak <v.spirydzionak@godeltech.com>
2024-01-13 11:57:24 +0300

    sixth commit

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -3,6 +3,14 @@ const exec = require('@actions/exec');
 const github = require('@actions/github');
 
 function run() {
+  // 1. Get input values
+  const bucket = core.getInput('bucket', {required: true});
+  const bucketRegion = core.getInput('bucket-region', {required: true});
+  const distFolder = core.getInput('dist-folder', {required: true});
+
+  // 2. Upload file to s3
+  const baseUri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${baseUri} --region ${bucketRegion}`);
   core.notice('Hello from my javascript action!')
 }
 
